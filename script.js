86431b0433a7da01496be2ff5e4ac6da6e504c86
AmirShariat <amishari@yahoo.com>
2024-02-02 21:52:16 +0330

    microTask vs EventLoop queue example

--- script.js
+++ script.js
@@ -43,54 +43,54 @@
 // getCountry("spain");
 // getCountry("japan");
 
-//Modern Way ES-6 Promise
-// const btn = document.querySelector(".btn-country");
-// const countriesContainer = document.querySelector(".countries");
-// function renderCountry(data, className = "") {
-// 	const html = `
-// 	<article class="country ${className}">
-// 		<img class="country__img" src="${data.flags.png}" />
-// 		<div class="country__data">
-// 		  <h3 class="country__name">${data.name.common}</h3>
-// 		  <h4 class="country__region">${data.region}</h4>
-// 		  <p class="country__row"><span>🏙️</span>${data.capital}</p>
-// 		  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
-// 				1
-// 			)} millions</p>
+Modern Way ES-6 Promise
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+function renderCountry(data, className = "") {
+	const html = `
+	<article class="country ${className}">
+		<img class="country__img" src="${data.flags.png}" />
+		<div class="country__data">
+		  <h3 class="country__name">${data.name.common}</h3>
+		  <h4 class="country__region">${data.region}</h4>
+		  <p class="country__row"><span>🏙️</span>${data.capital}</p>
+		  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+				1
+			)} millions</p>
 
-// 		  <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
-// 		  <p class="country__row"><span>💰</span>${
-// 				Object.values(data.currencies)[0].name
-// 			}</p>
-// 		</div>
-// 	</article>
-// 	`;
-// 	countriesContainer.insertAdjacentHTML("beforeend", html);
-// 	countriesContainer.style.opacity = 1;
-// }
+		  <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+		  <p class="country__row"><span>💰</span>${
+				Object.values(data.currencies)[0].name
+			}</p>
+		</div>
+	</article>
+	`;
+	countriesContainer.insertAdjacentHTML("beforeend", html);
+	countriesContainer.style.opacity = 1;
+}
 
-// function getCountryData(country) {
-// 	fetch(`https://restcountries.com/v3.1/name/${country}`)
-// 		.then(function (response) {
-// 			console.log(response);
-// 			return response.json();
-// 		})
-// 		.then(function (data) {
-// 			renderCountry(data[0]);
-// 			// for neighbors
-// 			const neighbors = data[0].borders[0];
-// 			if (!neighbors) return;
-// 			console.log(neighbors);
-// 			return fetch(`https://restcountries.com/v3.1/alpha/${neighbors}`);
-// 		})
-// 		.then((response) => response.json())
-// 		.then(function (data) {
-// 			renderCountry(data[0], "neighbor");
-// 		});
-// }
-// getCountryData("austria");
+function getCountryData(country) {
+	fetch(`https://restcountries.com/v3.1/name/${country}`)
+		.then(function (response) {
+			console.log(response);
+			return response.json();
+		})
+		.then(function (data) {
+			renderCountry(data[0]);
+			// for neighbors
+			const neighbors = data[0].borders[0];
+			if (!neighbors) return;
+			console.log(neighbors);
+			return fetch(`https://restcountries.com/v3.1/alpha/${neighbors}`);
+		})
+		.then((response) => response.json())
+		.then(function (data) {
+			renderCountry(data[0], "neighbor");
+		});
+}
+getCountryData("austria");
 
-// ES-6 Catching error centrally with refraction
+ES-6 Catching error centrally with refraction
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 function renderError(msg) {
@@ -113,6 +113,7 @@ function renderCountry(data, className = "") {
         Object.values(data.currencies)[0].name
       }</p>
 		</div>
+
 	</article>
 	`;
   countriesContainer.insertAdjacentHTML("beforeend", html);
@@ -142,4 +143,14 @@ function getCountryData(country) {
     .catch((err) => renderError(`Something went wrong! No neighbor exists!! `))
     .finally(() => (countriesContainer.style.opacity = 1)); // this line will executes regardless of promise result
 }
-getCountryData("romania");
+btn.addEventListener("click", () => getCountryData("poland"));
+
+// a proof for priority of executing microtasks queue over callback queue
+// console.log("Test");0
+// setTimeout(() => console.log("0s passed!"), 0);
+// Promise.resolve("resolved promise one!").then((res) => console.log(res));
+// Promise.resolve("resolved promise two!").then((res) => {
+//   for (let index = 0; index < 1_000_000_0000; index++) {}
+//   return console.log(res);
+// });
+// console.log("Test Ends!");
